a4d0eb45e0ba21c0738e0a04c18680259f9b691e
FrancyelZelling <zellingf@gmail.com>
2021-01-02 11:13:51 -0300

    Finished movie list and movie item styling

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -31,8 +31,9 @@ const MovieItem: React.FC<MovieItemProps> = ({ movie }) => {
       <img src={baseImageUrl + movie.poster_path} alt="" />
       <div className="details">
         <h2>{movie.title}</h2>
-        <button onClick={addToList}>Add To List</button>
-        <button onClick={removeMovie}>Remove From List</button>
+        <button onClick={addToList} className="btn-main">
+          Add To List
+        </button>
         <button>
           <Link to={`/movie/${movie.id}`}>View More</Link>
         </button>
